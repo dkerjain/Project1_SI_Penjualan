7232df2f77ab61c2a3fb671e415eb582225fbd0b
ayunindyarista <ayunindyarista@gmail.com>
2021-11-26 16:17:03 +0700

    update pemesanan belum selesai

--- resources/views/konten/transaksi/pemesanan.blade.php
+++ resources/views/konten/transaksi/pemesanan.blade.php
@@ -101,122 +101,122 @@
 
 
 <!-- /.modal Edit-->
-@foreach ($pemesanan as $p)
-  <div class="modal fade editpemesanan{{$p->id_pemesanan}}" id="modal-edit">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h4 class="modal-title">Detail Data Pemesanan</h4>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <form class="form-horizontal" action="/pemesanan/tambahpembayaran" enctype="multipart/form-data" method="post">
-            
-            <div class="modal-body">
-              {{ @csrf_field() }}
-                <input type="hidden" id="id_pemesanan" name="id_pemesanan" value="{{$p->id_pemesanan}}" class="form-control col-md-7 col-xs-12">
-                
-                <input type="hidden" id="total_bayar" name="total_bayar" value="{{$p->total_bayar}}" class="form-control col-md-7 col-xs-12">
-                <input type="hidden" id="jumlah" name="jumlah" value="{{$p->sisa}}" class="form-control col-md-7 col-xs-12">
-                
-
-              <div class="row">
-                <div class="col-sm-6">
-                    <div class="form-group">
-                    <label>ID Pemesanan</label>
-                    <input type="text" class="form-control" value="{{ $p->id_pemesanan}}" readonly>
-                    </div>
-                </div>
-
-                <div class="col-sm-6">
-                    <div class="form-group">
-                    <label>Total</label>
-                    <input type="text" class="form-control" value="Rp {{ number_format($p->total_biaya,2,',','.') }}" readonly>
-                    </div>
-                </div>
-              </div>
-
-              <table id="example2" class="table table-bordered table-hover">
-                <thead>
-                  <tr>
-                    <th>No</th>
-                    <th>Barang</th>
-                    <th>Ukuran Lensa</th>
-                    <th>Jenis Lensa</th>
-                    <th>Harga</th>
-                    <th>Jumlah</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  @foreach($detail as $d)
-                    @if(($p->id_pemesanan == $d->id_pemesanan) )
-                      <tr>
-                        <td>{{$loop->iteration}}</td>
-                        <td>{{$d->nama_barang}}</td>
-                        <td>{{$d->ukuran_lensa}}</td>
-                        <td>{{$d->jenis_lensa}}</td>
-                        <td>Rp {{ number_format($d->harga_kacamata,2,',','.') }}</td>
-                        <td>{{$d->jumlah_pemesanan}}</td>
-                      </tr>
-                    @endif
-                  @endforeach
-                </tbody>
-              </table>
-
-              @if($p->status_pembayaran == 1)
-              <div class="row">
-                <div class="col-sm-6">
-                    <div class="form-group">
-                    <label>Jumlah Bayar</label>
-                    <input type="text" class="form-control" readonly value="Rp {{ number_format($p->jumlah_bayar,2,',','.') }}" >
-                    </div>
-                </div>
-
-                <div class="col-sm-6">
-                    <div class="form-group">
-                    <label>Sisa Bayar</label>
-                    <input type="text" class="form-control" readonly value="Rp {{ number_format($p->sisa,2,',','.') }}" >
-                    </div>
-                </div>
-              </div>
+  @foreach ($pemesanan as $p)
+    <div class="modal fade editpemesanan{{$p->id_pemesanan}}" id="modal-edit">
+      <div class="modal-dialog modal-lg">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h4 class="modal-title">Detail Data Pemesanan</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+              </button>
             </div>
-            @else
-              <div class="row">
+            <form class="form-horizontal" action="/pemesanan/tambahpembayaran" enctype="multipart/form-data" method="post">
+              
+              <div class="modal-body">
+                {{ @csrf_field() }}
+                  <input type="hidden" id="id_pemesanan" name="id_pemesanan" value="{{$p->id_pemesanan}}" class="form-control col-md-7 col-xs-12">
+                  
+                  <input type="hidden" id="total_bayar" name="total_bayar" value="{{$p->total_bayar}}" class="form-control col-md-7 col-xs-12">
+                  <input type="hidden" id="jumlah" name="jumlah" value="{{$p->sisa}}" class="form-control col-md-7 col-xs-12">
+                  
+
+                <div class="row">
+                  <div class="col-sm-6">
+                      <div class="form-group">
+                      <label>ID Pemesanan</label>
+                      <input type="text" class="form-control" value="{{ $p->id_pemesanan}}" readonly>
+                      </div>
+                  </div>
+
+                  <div class="col-sm-6">
+                      <div class="form-group">
+                      <label>Total</label>
+                      <input type="text" class="form-control" value="Rp {{ number_format($p->total_biaya,2,',','.') }}" readonly>
+                      </div>
+                  </div>
+                </div>
+
+                <table id="example2" class="table table-bordered table-hover">
+                  <thead>
+                    <tr>
+                      <th>No</th>
+                      <th>Barang</th>
+                      <th>Ukuran Lensa</th>
+                      <th>Jenis Lensa</th>
+                      <th>Harga</th>
+                      <th>Jumlah</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($detail as $d)
+                      @if(($p->id_pemesanan == $d->id_pemesanan) )
+                        <tr>
+                          <td>{{$loop->iteration}}</td>
+                          <td>{{$d->nama_barang}}</td>
+                          <td>{{$d->ukuran_lensa}}</td>
+                          <td>{{$d->jenis_lensa}}</td>
+                          <td>Rp {{ number_format($d->harga_kacamata,2,',','.') }}</td>
+                          <td>{{$d->jumlah_pemesanan}}</td>
+                        </tr>
+                      @endif
+                    @endforeach
+                  </tbody>
+                </table>
+
+                @if($p->status_pembayaran == 1)
+                <div class="row">
                   <div class="col-sm-6">
                       <div class="form-group">
                       <label>Jumlah Bayar</label>
-                      <input type="text" class="form-control" readonly value="Rp {{ number_format($p->total_bayar,2,',','.') }}" >
+                      <input type="text" class="form-control" readonly value="Rp {{ number_format($p->jumlah_bayar,2,',','.') }}" >
                       </div>
                   </div>
 
                   <div class="col-sm-6">
                       <div class="form-group">
                       <label>Sisa Bayar</label>
-                      <input type="text" class="form-control" readonly value="Rp 0,00" >
+                      <input type="text" class="form-control" readonly value="Rp {{ number_format($p->sisa,2,',','.') }}" >
                       </div>
                   </div>
                 </div>
               </div>
-            @endif
+              @else
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                        <label>Jumlah Bayar</label>
+                        <input type="text" class="form-control" readonly value="Rp {{ number_format($p->total_bayar,2,',','.') }}" >
+                        </div>
+                    </div>
 
-            <div class="modal-footer justify-content-between">
-             
-              @if($p->status_pembayaran == 1)
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-success">Tambah Pembayaran</button>
+                    <div class="col-sm-6">
+                        <div class="form-group">
+                        <label>Sisa Bayar</label>
+                        <input type="text" class="form-control" readonly value="Rp 0,00" >
+                        </div>
+                    </div>
+                  </div>
+                </div>
               @endif
-              @if($p->status_pembayaran == 0)
-                
-              @endif
-            </div>
-            
-          </form>
-        </div>
+
+              <div class="modal-footer justify-content-between">
+              
+                @if($p->status_pembayaran == 1)
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                  <button type="submit" class="btn btn-success">Tambah Pembayaran</button>
+                @endif
+                @if($p->status_pembayaran == 0)
+                  
+                @endif
+              </div>
+              
+            </form>
+          </div>
+      </div>
+      <!-- /.modal-dialog -->
     </div>
-    <!-- /.modal-dialog -->
-  </div>
-@endforeach
+  @endforeach
 <!-- /.modal -->
       
 @endsection
